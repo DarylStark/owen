3aeb40fc1f8fae607cded1d797c36d405e19550d
Daryl Stark <daryl@dstark.nl>
2022-02-05 14:05:20 +0100

    Created "card"

---
+++ owen.py
@@ -0,0 +1,22 @@
+class Baby:
+    def __init__(self, mom, dad) -> None:
+        pass
+
+
+from datetime import datetime
+from maes import Birgit
+from stark import Daryl
+
+Owen = Baby(mom=Birgit, dad=Daryl)
+Owen.birth_datetime = datetime(year=2022,
+                               month=1,
+                               day=20,
+                               hour=21,
+                               minute=42)
+Owen.gender = "Boy"
+Owen.weight_grams = 3786
+Owen.length_cm = 51
+Owen.cute = True
+
+Owen.hug()
+
